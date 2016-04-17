eb598918f698264e3304ca5e84f6517c69c05f7e
michiwieland <michi.wieland@hotmail.com>
2016-04-17 18:24:43 +0200

    display mobile submenus / toggle visibility of wiki page parts (not finished yet, div is missing)

--- js/script.js
+++ js/script.js
@@ -2,6 +2,25 @@ $(document).ready(function() {
 	// Toggle mobile menu
 	$( "#hamburger" ).click(function() {
 		$( "nav" ).toggle( "slow" );
-		$( ".social").toggle(300);
+		$( ".social" ).toggle(300);
+	});
+
+	// Toggle sub mobile menu
+	$( "nav > ul > li" ).click(function(event) {
+		if($( "#hamburger" ).is(":visible")){
+			event.preventDefault();
+			$(this).siblings( "nav > ul > li " ).removeClass("active");
+			$(this).siblings( "nav > ul > li " ).each(function(){
+				$(this).find( "ul" ).hide();
+			});
+			$(this).toggleClass( "active" );
+			$(this).find( "ul" ).first().toggle("fast");
+		}
+	});
+
+	// Content preview for wiki pages
+	$( ".wiki-content h2" ).click(function(){
+		$(this).next( "div" ).toggle();
+		$(this).toggleClass( "arrow" );
 	});
 });
