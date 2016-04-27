4163c570bb95b0693609cdddf08eea3c1ad98106
michiwieland <michi.wieland@hotmail.com>
2016-04-27 22:55:51 +0200

    scroll to top only visible when needed, missspelling fixed, unfinished documentation removed

--- js/script.js
+++ js/script.js
@@ -106,3 +106,11 @@ $(document).ready(function() {
 		}
 	});
 });
+
+$(window).scroll(function(){
+	if($(window).scrollTop() > 100){
+		$("#scroll-top").fadeIn("slow");
+	} else {
+		$("#scroll-top").fadeOut("slow");
+	}
+});
